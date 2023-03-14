f73effe7b194179cffb7b40905ffa4b23b3b579d
Shayan Shaikh <shayan.shaikh@coolbet.com>
2023-03-14 15:35:02 +0200

    Create request for appointment

--- src/containers/appointments/Appointments.jsx
+++ src/containers/appointments/Appointments.jsx
@@ -8,12 +8,61 @@ import {
   Button,
   message,
   Typography,
+  Table,
 } from "antd";
 import PageHeader from "../../main/components/PageHeader";
-import Table from "./components/Table";
 import Filters from "./components/Filters";
+import sendRequest from "../../api_helpers/requests/sendRequest";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 function Appointments() {
+  const baseUrl = useSelector((state) => state.common.url);
+  const [dataSource, setDataSource] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const columns = [
+    {
+      title: "Appointment id",
+      dataIndex: "appointment_id",
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+    },
+    {
+      title: "Customer Name",
+      dataIndex: "customer_name",
+    },
+    {
+      title: "Staff Name",
+      dataIndex: "staff_name",
+    },
+  ];
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const httpRequestData = {
+        url: `${baseUrl}/getAppointments`,
+        method: "POST",
+        postData: {
+          action: "getAppointments",
+          storeId: "1",
+        },
+      };
+      const response = await sendRequest(httpRequestData);
+      setDataSource(response);
+      setLoading(false);
+    } catch (e) {
+      setDataSource([]);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Row>
       <Col span={24}>
@@ -22,7 +71,7 @@ function Appointments() {
       <Col span={24}>
         <Card>
           <Filters />
-          <Table />
+          <Table columns={columns} dataSource={dataSource} />
         </Card>
       </Col>
     </Row>
--- src/containers/appointments/components/Table.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AppointmentTable() {
-  return (
-    <div>AppointmentTable</div>
-  )
-}
