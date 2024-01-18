be0238ec74f7aaa44f1ea03388ebc2b44917a607
Shayan Shaikh <shayanshaikh996@gmail.com>
2024-01-18 15:35:04 +0200

    Removed irrelevant turbo log code

--- src/main/reducers/index.js
+++ src/main/reducers/index.js
@@ -1,8 +1,7 @@
 import { common } from "./Common";
 import { wizard } from "./Wizard";
-import { turbolog } from "./TurboLog";
 import { combineReducers } from "redux";
 
-const allReduces = combineReducers({ common, wizard, turbolog });
+const allReduces = combineReducers({ common, wizard });
 
 export default allReduces;
